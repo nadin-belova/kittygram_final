ffbef1e415318bb98e95df9d9fc33059ec0ca510
Надежда Белова <belovanadezhda1983@gmail.com>
2023-08-23 16:22:31 +0300

    SECRET_KEY = os.getenv("SECRET_KEY")

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = os.environ.get("DEBUG", False)
 
